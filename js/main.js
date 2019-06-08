cd2ebef360ec105a9db8982789a5375dfed6bd47
Carlos Fuentes <fuentesc91@gmail.com>
2019-06-08 14:43:52 -0500

    fix scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const $mainBackground = document.getElementById('mainBackground');
 
 window.onscroll = () => {
-  console.log(window.scrollY);
-  $mainBackground.style.backgroundPosition = `50% -${window.scrollY/3}px`
+  $mainBackground.style.backgroundPosition = `50% -${window.scrollY/4}px`
 }
