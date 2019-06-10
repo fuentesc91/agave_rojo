90d4e9649eed26b28a1ecebb37e83ec2ef1f64c2
Carlos Fuentes <fuentesc91@gmail.com>
2019-06-09 19:12:55 -0500

    Resuelto el movimiento del fondo de pantalla

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-const $mainBackground = document.getElementById('mainBackground');
+const $backgrounds = document.getElementsByClassName('background-image');
+
+// Asign all the backgrounds to the backgrouds divs
+Array.from($backgrounds).forEach((background, i) => {
+  background.style.background = `url(src/img/background${i}.jpg) fixed center no-repeat`;
+  background.style.backgroundSize = 'cover';
+})
+//function to position backgrounds where they should be in that moment
+function setBackgroundPosition($backgrounds) {
+  Array.from($backgrounds).forEach((background, i) => {
+    background.style.backgroundPositionY = `${(background.offsetTop - window.scrollY)/2.5}px`;
+  })
+}
+
+setBackgroundPosition($backgrounds);
 
 window.onscroll = () => {
-  $mainBackground.style.backgroundPosition = `50% -${window.scrollY/4}px`
+  setBackgroundPosition($backgrounds);
 }
